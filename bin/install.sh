82765034fc3b5f1d4adf6ad3e5c74b02e3dc521f
Sandor Semsey <sandor@es-progress.hu>
2022-04-03 19:50:01 +0200

    add vhost

--- bin/install.sh
+++ bin/install.sh
@@ -26,13 +26,37 @@ install_dir="${1?:'Install dir missing'}"
 db_user_name="${2?:'DB user name missing'}"
 db_user_pass="${3?:'DB user pass missing'}"
 db_name="${4?:'DB name missing'}"
+routing="127.0.0.1 ${civi_domain}"
+doc_root="${install_dir}/web"
 
 print-header "Purge instance..."
 sudo mysql -u"${db_user_name}" -p"${db_user_pass}" -e "DROP DATABASE IF EXISTS ${db_name};"
 sudo rm -rf "${install_dir}/web/sites/default/civicrm.settings.php" "${install_dir}/web/sites/default/settings.php" "${install_dir}/web/sites/default/files/"
 print-finish
 
-pwd
+print-header "Add Civi vhost..."
+# Routing
+if ! grep -qs "${routing}" /etc/hosts; then
+    echo "${routing}" | sudo tee -a /etc/hosts >/dev/null
+fi
+# Document root
+sudo mkdir -p "${doc_root}"
+sudo chgrp -R www-data "${install_dir}"
+# Vhost
+sudo cp "${install_dir}/vhost.conf" "/etc/apache2/sites-available/${civi_domain}.conf"
+sudo sed -i \
+    -e "s@{{ site }}@${civi_domain}@g" \
+    -e "s@{{ doc_root }}@${doc_root}@g" \
+    "/etc/apache2/sites-available/${civi_domain}.conf"
+sudo a2ensite "${civi_domain}.conf"
+sudo systemctl reload apache2.service
+print-finish
+
+# Testing
+echo puruttya | sudo tee -a "${doc_root}/majom" >/dev/null
+sudo chgrp -R www-data "${doc_root}/majom"
 ls -lah "${install_dir}"
+ls -lah "${install_dir}/web"
+curl "http://${civi_domain}/majom"
 
 exit 0
