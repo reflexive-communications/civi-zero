1a30d3cc8d2b5154a097b317b9b2fb40434f700e
Sandor Semsey <sandor@es-progress.hu>
2022-04-04 01:34:32 +0200

    tests

---
+++ bin/tests.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+####################
+## civi-zero      ##
+##                ##
+## Run unit tests ##
+####################
+
+# Strict mode
+set -euo pipefail
+IFS=$'\n\t'
+
+# Include library
+base_dir="$(builtin cd "$(dirname "${0}")" >/dev/null 2>&1 && pwd)"
+. "${base_dir}/library.sh"
+
+# Include configs
+. "${base_dir}/../install.conf"
+[[ -r "${base_dir}/../install.local" ]] && . "${base_dir}/../install.local"
+
+# Parse options
+install_dir="${1?:'Install dir missing'}"
+extension="${2?:'Extension missing'}"
+extension_target="${install_dir}/web/extensions"
+
+print-header "Run unit tests (${extension})"
+sudo chown -R "${USER}" "${install_dir}/web/"
+cd "${extension_target}/${extension}"
+XDEBUG_MODE=coverage phpunit --verbose --coverage-text
+print-finish
+
+exit 0
