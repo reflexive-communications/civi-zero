a904378bd92d624400a0cd6951e42f9187a2fcb5
Sandor Semsey <sandor@es-progress.hu>
2022-04-03 13:06:38 +0200

    install PHP

--- bin/ci-prepare.sh
+++ bin/ci-prepare.sh
@@ -5,6 +5,12 @@
 ## Setup CI environment ##
 ##########################
 
+############
+## CONFIG ##
+############
+web_root=/srv/www/
+php_version="7.4"
+
 #################
 ## SCRIPT START #
 #################
@@ -17,8 +23,40 @@ IFS=$'\n\t'
 base_dir="$(builtin cd "$(dirname "${0}")" >/dev/null 2>&1 && pwd)"
 source "${base_dir}/library.sh"
 
+print-header "Add apt repository"
+sudo add-apt-repository --yes --no-update ppa:ondrej/apache2
+sudo add-apt-repository --yes --no-update ppa:ondrej/php
+sudo apt update
+print-finish
+
 print-header "Install Apache..."
-sudo add-apt-repository --yes ppa:ondrej/apache2
 sudo apt-get install --yes --no-install-recommends apache2 libapache2-mod-fcgid libapache2-mod-security2
 sudo a2enmod actions expires headers rewrite
+sudo systemctl enable apache2.service
+sudo systemctl restart apache2.service
+sudo mkdir -p "${web_root}"
+print-finish
+
+print-header "Install PHP..."
+sudo apt-get install --yes --no-install-recommends \
+    "php${php_version}" \
+    "php${php_version}-bcmath" \
+    "php${php_version}-bz2" \
+    "php${php_version}-cli" \
+    "php${php_version}-common" \
+    "php${php_version}-curl" \
+    "php${php_version}-fpm" \
+    "php${php_version}-gd" \
+    "php${php_version}-imagick" \
+    "php${php_version}-imap" \
+    "php${php_version}-intl" \
+    "php${php_version}-json" \
+    "php${php_version}-ldap" \
+    "php${php_version}-mbstring" \
+    "php${php_version}-mysql" \
+    "php${php_version}-opcache" \
+    "php${php_version}-soap" \
+    "php${php_version}-zip" \
+    "php${php_version}-xdebug" \
+    "php${php_version}-xml"
 print-finish
