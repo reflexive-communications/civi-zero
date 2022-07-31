4a06c243b97adbe5ed017f1bf4038b39ea48cb3c
Sandor Semsey <sandor@es-progress.hu>
2022-07-31 15:20:17 +0200

    use config files from base_dir

--- bin/install.sh
+++ bin/install.sh
@@ -57,7 +57,7 @@ fi
 mkdir -p "${doc_root}"
 sudo chgrp -R www-data "${install_dir}"
 # Vhost
-sudo cp "${install_dir}/cfg/vhost.conf" "/etc/apache2/sites-available/${civi_domain}.conf"
+sudo cp "${base_dir}/cfg/vhost.conf" "/etc/apache2/sites-available/${civi_domain}.conf"
 sudo sed -i \
     -e "s@{{ site }}@${civi_domain}@g" \
     -e "s@{{ doc_root }}@${doc_root}@g" \
